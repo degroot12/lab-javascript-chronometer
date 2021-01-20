908430d84dab34fe1e61c35f681a5ec5020f2d13
Thomas de Groot <degroot12@gmail.com>
2021-01-20 21:20:50 +0100

    no bonus yet

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,52 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0
   }
   startClick(callback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if(callback){
+        callback()
+      };
+    }, 1000);
     // ... your code goes here
   }
   getMinutes() {
     // ... your code goes here
+    let secondsToMinutes = Math.floor(this.currentTime / 60)
+    return secondsToMinutes
   }
   getSeconds() {
     // ... your code goes here
+    let remainingSeconds = this.currentTime % 60;
+    return remainingSeconds
   }
-  twoDigitsNumber() {
+  
+  twoDigitsNumber(secMin) {
+    
+    if(secMin < 10){
+      return `0` + `${secMin}`
+    } else {
+      return `${secMin}`
+    }
     // ... your code goes here
   }
+
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
+    this.currentTime = 0;
     // ... your code goes here
   }
   splitClick() {
+
+    let minutes = this.twoDigitsNumber(this.getMinutes());
+    let seconds = this.twoDigitsNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
     // ... your code goes here
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -15,14 +15,25 @@ let splits = document.getElementById('splits');
 
 function printTime() {
   // ... your code goes here
+  printMinutes();
+  printSeconds();
 }
 
 function printMinutes() {
   // ... your code goes here
+  let minutes = chronometer.twoDigitsNumber(chronometer.getMinutes())
+  minDec.innerText = minutes[0];
+  minUni.innerText = minutes[1];
+
 }
 
 function printSeconds() {
   // ... your code goes here
+  let seconds = chronometer.twoDigitsNumber(chronometer.getSeconds());
+  //console.log(seconds)
+  secUni.innerText = seconds[1];
+  secDec.innerText = seconds[0];
+  
 }
 
 // ==> BONUS
@@ -32,34 +43,89 @@ function printMilliseconds() {
 
 function printSplit() {
   // ... your code goes here
+  //let olList = document.querySelector('ol');
+  let listSplit = document.createElement('li');
+  listSplit.innerText = chronometer.splitClick();
+  console.log(listSplit.innerText)
+  splits.appendChild(listSplit)
+
 }
 
 function clearSplits() {
   // ... your code goes here
+  splits.innerText = '';
+  secUni.innerText = '0';
+  secDec.innerText = '0';
+  minDec.innerText = '0';
+  minUni.innerText = '0';
+
+
 }
 
 function setStopBtn() {
   // ... your code goes here
+  btnLeft.className = 'btn stop';
+  btnLeft.innerText = 'STOP'
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  btnRight.className = 'btn split';
+  btnRight.innerText = 'SPLIT'
 }
 
 function setStartBtn() {
   // ... your code goes here
+  btnLeft.className = 'btn start'
+  btnLeft.innerText = 'START'
 }
 
 function setResetBtn() {
   // ... your code goes here
+  btnRight.className = 'btn reset'
+  btnRight.innerText = 'RESET'
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnLeft.classList.contains('start')){
+    chronometer.startClick(printTime, printMilliseconds);
+    setStopBtn();
+    setSplitBtn()
+  } else {
+    chronometer.stopClick();
+    setStartBtn();
+    setResetBtn();
+  }
 });
 
 // Reset/Split Button
 btnRight.addEventListener('click', () => {
   // ... your code goes here
+  if(btnRight.classList.contains('reset')){
+    clearSplits()
+    chronometer.resetClick()
+  } else {
+    printSplit()
+  }
 });
+
+
+// function handleForm(){
+//   let form = document.querySelector('#myForm')
+//   let olList = document.querySelector('ol')
+
+
+//   // some default behaviour
+//   form.addEventListener('submit', function(event){
+//       event.preventDefault()
+//       let name = document.querySelector('#name')
+//       console.log(name.value)
+//       let item = document.createElement('li')
+//       item.innerText = name.value
+//       console.log(item)
+
+//       ulList.appendChild(item)
+
+//   })
+// }
